5995d830b76dbf3c7f1c6fa623776e610a4a4c79
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-12 14:25:49 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -15,3 +15,17 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// // const React=require('react')
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter} from "react-router-dom";
+// import App from './App';
+// ReactDOM.render(
+//     <BrowserRouter>
+// {/* <Route path="/"  component={App} /> */}
+// <App/>
+// </BrowserRouter>,
+// document.getElementById('root'))
+// ;
