068881e98a3f2808ae58a4f698e9209cc0767751
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-13 19:51:06 +0300

    sjh

--- src/component/Search/index.js
+++ src/component/Search/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 // import { useHistory } from "react-router-dom";
-// import { useHistory } from "react-router-dom";
+
 import { useNavigate } from "react-router-dom";
 const BASE_URL = "http://localhost:4000";
 
