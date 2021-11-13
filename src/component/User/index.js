bfd906725b6d5d280c90306bc52d9aaf751edcd5
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-13 23:27:12 +0300

    new

--- src/component/User/index.js
+++ src/component/User/index.js
@@ -4,6 +4,8 @@ import "./style.css";
 const BASE_URL = "http://localhost:4000";
 const User = () => {
   const [users, setusers] = useState([]);
+  const [AddinputSignvalue, setAddinputSignvalue] = useState("");
+  const [AddinputLogvalue, setAddinputLogvalue] = useState("");
 
   useEffect(() => {
     getAllUsers();
@@ -14,35 +16,92 @@ const User = () => {
 
     setusers(users.data);
   };
-  const createNew = (e) => {
-    e.preventDefault();
-    console.log("create");
+  const createNew = () => {
+    //   console.log("create");
 
     let obj = {
       id: users.length + 1,
-      userName: e.target.inside.value,
+      userName: AddinputSignvalue,
       Fav: [],
     };
-    // setusers([...users , obj])
-
 
     axios
       .post(`${BASE_URL}/users/addNew`, obj)
-      .then(() => console.log("yess User Created "))
+      .then(() => console.log(" User Created "))
       .catch((err) => {
         console.error(err);
       });
-      e.target.inside.value = "";
+
     console.log(users);
   };
 
+  const logOut = () => {
+    localStorage.clear();
+    console.log("log out");
+  };
+
+  let userId = JSON.parse(localStorage.getItem("userId"));
+
   return (
-    <div>
-      <form onSubmit={createNew}>
-        <button> sign up </button>{" "}
-        <input type="text" placeholder="enter your name" name="inside" />
-      </form>
-    </div>
+    <>
+      {userId ? (
+        <form onSubmit={logOut}>
+          <button> log out </button>
+        </form>
+      ) : (
+        <div>
+          <button
+            onClick={() => {
+              let found = users.find((ele) => {
+                return ele.name == AddinputSignvalue;
+              });
+              if (found ) {
+                return (
+                  <p>
+                    you already have an account! log in or change your username{" "}
+                  </p>
+                );
+              } else {
+                createNew();
+              }
+            }}
+          >
+            {" "}
+            sign up{" "}
+          </button>{" "}
+          <input
+            onChange={(e) => {
+              setAddinputSignvalue(e.target.value);
+            }}
+            type="text"
+            placeholder="enter your name"
+          />
+          <button
+            onClick={() => {
+              let found = users.find((ele) => {
+                return ele.name == AddinputLogvalue;
+              });
+              if (found) {
+                localStorage.setItem("userId", JSON.stringify(found.id));
+                return <p>your in </p>;
+              } else {
+                return <p>you don't have an acount!, sign up </p>;
+              }
+            }}
+          >
+            {" "}
+            log in{" "}
+          </button>{" "}
+          <input
+            onChange={(e) => {
+              setAddinputLogvalue(e.target.value);
+            }}
+            type="text"
+            placeholder="enter your name"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
