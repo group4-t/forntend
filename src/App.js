7ccf5a11bbbfdbf83410557fa524564484a521c3
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-13 17:01:04 +0300

    addsaresh

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Movies from "./component/Movies";
 import Movie from "./component/Movie";
 import OnePodcast from "./component/onepodcast";
 import Home from "./component/Home";
+import Search from "./component/Search";
 import OneMusic from  "./component/OneMusic";
 import HomeItem from "./component/homeItem/homeItem";
 // import axios from "axios";
@@ -23,21 +24,22 @@ const App = () => {
       <Nav />
       {/* >>>>>>> 1e3c1e70b26cb15df472e9ffa1de8844ab086d6a */}
       <Routes>
-      <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/Music" element={<Music />} />
         <Route exact path="/Podcast" element={<Podcast />} />
         <Route exact path="/Movies" element={<Movies />} />
         <Route exact path="/Ebook" element={<Ebook />} />
         <Route exact path="/Fav" element={<Fav />} />
         <Route exact path="/user" element={<User />} />
+        <Route exact path="/search" element={<Search />} />
+
         <Route exact path="/Music/:trackId" element={<OneMusic />} />
         <Route exact path="/Movie/:trackId" element={<Movie />} />
         <Route exact path="/:trackId" element={<HomeItem />} />
-   
-        <Route exact path="/Podcast/:trackId" element={<OnePodcast />} />
 
+        <Route exact path="/Podcast/:trackId" element={<OnePodcast />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 };
--- src/component/Nav/index.js
+++ src/component/Nav/index.js
@@ -1,10 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
 // import { useNavigate } from 'react-router';
 import "./style.css";
 import head from "./../../imges/head.png";
+import React from "react";
 
 const Nav = () => {
+
+  
   return (
     <div className="nav">
       <ul className="ulNav">
@@ -33,6 +35,12 @@ const Nav = () => {
             Movies
           </Link>
         </li>
+        <li className="lii">
+          <Link className="linkk" to="/search">
+            Search
+          </Link>
+        </li>
+        
         {/* <li>
           <Link  className="linkk" to="/Ebook">Ebooks</Link>
         </li> */}
