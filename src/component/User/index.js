430a53df3c340ccdd0a5776016cb05b794c4e5bc
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-14 10:22:54 +0300

    l

--- src/component/User/index.js
+++ src/component/User/index.js
@@ -40,73 +40,82 @@ const User = () => {
     <>
       {userId ? (
         <form onSubmit={logOut}>
-          <button> log out </button>
+          <button className="userBtn"> log out </button>
         </form>
       ) : (
-        <div>
-          <button
-            onClick={() => {
-              let found = users.find((ele) => {
-                return ele.userName == AddinputSignvalue;
-              });
+        <div className="contenerB">
+          <div className="firstDiv">
+            <button
+              className="userBtn"
+              onClick={() => {
+                let found = users.find((ele) => {
+                  return ele.userName == AddinputSignvalue;
+                });
 
-              if (found) {
-                console.log(found);
-                return (
-                  <p className="accountText">
-                    {console.log(
-                      "you already have an account! log in or change your username"
-                    )}
-                    you already have an account! log in or change your username{" "}
-                  </p>
-                );
-              } else {
-                localStorage.setItem(
-                  "userId",
-                  JSON.stringify(users.length + 1)
-                );
-                console.log(users.length + 1);
-                createNew();
-                window.location.reload(false);
-              }
-            }}
-          >
-            {" "}
-            sign up{" "}
-          </button>{" "}
-          <input
-            onChange={(e) => {
-              setAddinputSignvalue(e.target.value);
-            }}
-            type="text"
-            placeholder="enter your name"
-          />
-          <button
-            onClick={() => {
-              let found = users.find((ele) => {
-                return ele.userName == AddinputLogvalue;
-              });
-              if (found) {
-                localStorage.setItem("userId", JSON.stringify(found.id));
-                console.log("your in ");
-                window.location.reload(false);
-                return <p>your in </p>;
-              } else {
-                console.log("you dont have an account");
-                return <p>you don't have an acount!, sign up </p>;
-              }
-            }}
-          >
-            {" "}
-            log in{" "}
-          </button>{" "}
-          <input
-            onChange={(e) => {
-              setAddinputLogvalue(e.target.value);
-            }}
-            type="text"
-            placeholder="enter your name"
-          />
+                if (found) {
+                  console.log(found);
+                  return (
+                    <p className="accountText">
+                      {console.log(
+                        "you already have an account! log in or change your username"
+                      )}
+                      you already have an account! log in or change your
+                      username{" "}
+                    </p>
+                  );
+                } else {
+                  localStorage.setItem(
+                    "userId",
+                    JSON.stringify(users.length + 1)
+                  );
+                  console.log(users.length + 1);
+                  createNew();
+                  window.location.reload(false);
+                }
+              }}
+            >
+              {" "}
+              sign up{" "}
+            </button>{" "}
+            <input
+              className="inputt1"
+              onChange={(e) => {
+                setAddinputSignvalue(e.target.value);
+              }}
+              type="text"
+              placeholder="enter your name"
+            />
+          </div>
+          <div className="secondDiv">
+            <button
+              className="userBtn"
+              onClick={() => {
+                let found = users.find((ele) => {
+                  return ele.userName == AddinputLogvalue;
+                });
+                if (found) {
+                  localStorage.setItem("userId", JSON.stringify(found.id));
+                  console.log("your in ");
+                  window.location.reload(false);
+                  return <p>your in </p>;
+                } else {
+                  console.log("you dont have an account");
+                  return <p>you don't have an acount!, sign up </p>;
+                }
+              }}
+            >
+              {" "}
+              log in{" "}
+            </button>{" "}
+            <input
+              className="inputt2"
+              onChange={(e) => {
+                setAddinputLogvalue(e.target.value);
+              }}
+              type="text"
+              placeholder="enter your name"
+            />
+          </div>
         </div>
       )}
     </>
